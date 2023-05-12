d17e4cc4f4b1c52151c4e6059299fd8eae4d4345
naomiizhong <117184480+naomiizhong@users.noreply.github.com>
2023-05-12 09:08:09 -0400

    Update main.c

--- main.c
+++ main.c
@@ -55,7 +55,7 @@ int main(int argc, char const *argv[])
 
     printf("\nHello %s\n", name); // This is a function call, it calls the printf function, which prints the name variable to the command line
 
-    if(number > 21) { // C uses the basic conditions of >=, >, <=, <, ==, != (not equal to). For logical operators it uses && (and), || (or), ! (not)
+    if(number >= 21) { // C uses the basic conditions of >=, >, <=, <, ==, != (not equal to). For logical operators it uses && (and), || (or), ! (not)
         printf("You are old enough to drink in the US\n"); // This is a function call, it calls the printf function, it needs double quoted strings
     } else {
         printf("You are not old enough to drink in the US\n"); // This is a function call, it calls the printf function
